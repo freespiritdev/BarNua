2f2720a3b2b69858d5968460446cacdc3c699b37
Cheri <avyishi@gmail.com>
2015-11-16 23:19:42 -0800

    Added jquery image slider

--- js/app.js
+++ js/app.js
@@ -1,14 +1,12 @@
-function init_map(){
-  var myOptions = {
-    zoom:15,center:new google.maps.LatLng(37.8000056,-122.41002409999999),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById("gmap_canvas"), myOptions);
-  marker = new google.maps.Marker({
-    map: map,position: new google.maps.LatLng(37.8000056, -122.41002409999999)
-  });infowindow = new google.maps.InfoWindow({
-    content:"BarNua<br/>561 Columbus Ave<br/>94133 san francisco" 
+$( document ).ready(function() {
+(function($) {
+jQuery(window).load(function() {
+  $('.flexslider').flexslider({
+    animation: "slide",
+    slideshow: true,
+    direction: "horizontal"
   });
-  google.maps.event.addListener(marker, "click", function(){
-    infowindow.open(map,marker);});infowindow.open(map,marker);
-}google.maps.event.addDomListener(window, 'load', init_map);
+});
+})(jQuery);
+});
+
