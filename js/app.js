1ca809030266961898634ec2224d3d02632d3ed3
Cheri <avyishi@gmail.com>
2015-12-07 19:57:51 -0800

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 $( document ).ready(function() {
-(function($) {
-jQuery(window).load(function() {
-  $('.flexslider').flexslider({
-    animation: "slide",
-    slideshow: true,
-    directionNav: false,
-    direction: "vertical"
-  });
-});
-})(jQuery);
+  (function($) {
+    jQuery(window).load(function() {
+      $('.flexslider').flexslider({
+        animation: "slide",
+        slideshow: true,
+        directionNav: false,
+        direction: "vertical"
+      });
+    });
+  })(jQuery);
 });
 
 
